f3ebabb3618eb192d018f7e153f91cd50a0b0e39
Fiction Animated <fictionanimated@gmail.com>
2025-06-27 23:41:44 +0000

    it's still same, feels hanged for few seconds

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,8 @@ import { Loader } from 'lucide-react';
 
 export default function Loading() {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <Loader className="h-8 w-8 animate-spin text-primary" />
+    <div className="absolute inset-0 z-50 flex items-center justify-center bg-background/50 backdrop-blur-sm">
+      <Loader className="h-12 w-12 animate-spin text-primary" />
     </div>
   );
 }
