8aa4b2c97b6a81dacdd3451506bfa00815a7791e
Fiction Animated <fictionanimated@gmail.com>
2025-07-30 00:38:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,12 +1,14 @@
+// This approach is inspired by the official Next.js + MongoDB example
+// https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
 
-import { MongoClient, type MongoClientOptions } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 const uri = process.env.MONGODB_URI;
-const options: MongoClientOptions = {};
+const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
@@ -15,8 +17,8 @@ if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>
-  }
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
 
   if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
